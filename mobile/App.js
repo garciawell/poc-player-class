8285a0be1c91f5977ef03b8d477c3a6df1b58f91
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-06-02 12:23:02 -0300

    poc start

---
+++ mobile/App.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import {WebView} from 'react-native-webview';
+
+import {
+  Dimensions,
+  Button,
+  PermissionsAndroid,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const requestCameraPermission = async () => {
+    // try {
+    //   const granted = await PermissionsAndroid.request(
+    //     PermissionsAndroid.PERMISSIONS.CAMERA,
+    //     PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    //     {
+    //       title: 'Cool Photo App Camera Permission',
+    //       message:
+    //         'Cool Photo App needs access to your camera ' +
+    //         'so you can take awesome pictures.',
+    //       buttonNeutral: 'Ask Me Later',
+    //       buttonNegative: 'Cancel',
+    //       buttonPositive: 'OK',
+    //     },
+    //   );
+    //   if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    //     console.log('You can use the camera');
+    //   } else {
+    //     console.log('Camera permission denied');
+    //   }
+    // } catch (err) {
+    //   console.warn(err);
+    // }
+  };
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      {/* <Button title="request permissions" onPress={requestCameraPermission} /> */}
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <WebView
+            source={{
+              uri: 'https://garcia.whereby.com/e24f501b-0d97-4f72-b36c-f471e7cbfa85?embed&chat=on&screenshare=on&lang=pt&people=on&background=on&breakout&skipMediaPermissionPrompt',
+            }}
+            style={{flex: 1, height: Dimensions.get('window').height}}
+            allowAud
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
