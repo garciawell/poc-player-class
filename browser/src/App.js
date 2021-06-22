c8f79be480d062616c0c9c08ce35962cbd5d3f7b
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-06-22 15:16:00 -0300

    change fix

--- browser/src/App.js
+++ browser/src/App.js
@@ -10,7 +10,7 @@ function App() {
               {/* <img src={logo} className="App-logo" alt="logo" /> */}
               <iframe
               title="conference"
-              src="https://lucasf3000.whereby.com/314d6bac-8aa3-48e9-b1d6-ac5625a6ce42?roomKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWV0aW5nSWQiOiIzNDU3NzEwMSIsInJvb21SZWZlcmVuY2UiOnsicm9vbU5hbWUiOiIvMzE0ZDZiYWMtOGFhMy00OGU5LWIxZDYtYWM1NjI1YTZjZTQyIiwib3JnYW5pemF0aW9uSWQiOiIxMTU2MzUifSwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5zcnYud2hlcmVieS5jb20iLCJpYXQiOjE2MjQzODQ0MzQsInJvb21LZXlUeXBlIjoibWVldGluZ0hvc3QifQ.M-i4DF7SI5_deHU92xmm-IMwB1UOZSV5W-wSZDVmfKo?embed&chat=on&screenshare=on&lang=pt&people=on&background=on&breakout"
+              src="https://lucasf3000.whereby.com/314d6bac-8aa3-48e9-b1d6-ac5625a6ce42?embed&chat=on&screenshare=on&lang=pt&people=on&background=on&breakout"
               allow="camera; microphone; fullscreen; speaker; display-capture"
             ></iframe>
           </div>
